6352fb732c6e698991a78b57eca8e649580b88e1
luxor94 <47752490+luxor94@users.noreply.github.com>
2021-02-01 22:53:52 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-   return typeof str === 'boolean';
-}
+module.exports = 
+  function check(str, bracketsConfig) {
+    bracketsConfig = bracketsConfig.map(function(x) { return x.join('');  })
+    
+  
+      function brackets(str) {
+        let newStr = str
+        for (let i = 0; i < bracketsConfig.length; i++) {
+          newStr = newStr.replace(bracketsConfig[i], '')
+        }
+        if (newStr === str) {
+          return newStr
+        } else {
+         return  brackets(newStr) 
+        }
+  
+  
+    }
+    
+      const result = brackets(str)
+      
+      if (result.length == 0) {
+        return true
+      } else return false
+  }
